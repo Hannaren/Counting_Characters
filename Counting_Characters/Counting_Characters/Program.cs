3d5db81aa060986882d20a0996e5abd06a0fdef8
Hanna Sumpter <sumpterhanna@gmail.com>
2020-10-12 11:12:22 -0500

    fixing git status errors

--- Counting_Characters/Counting_Characters/Program.cs
+++ Counting_Characters/Counting_Characters/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Counting_Characters
 {
@@ -6,7 +7,17 @@ namespace Counting_Characters
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter string to count: ");
+            string input = Console.ReadLine();
+
+          //  string loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc accumsan sem ut ligula scelerisque sollicitudin. Ut at sagittis augue. Praesent quis rhoncus justo. Aliquam erat volutpat. Donec sit amet suscipit metus, non lobortis massa. Vestibulum augue ex, dapibus ac suscipit vel, volutpat eget massa. Donec nec velit non ligula efficitur luctus.";
+
+            Dictionary<char, int> countedCharacters = Counter.charCounter(input);
+            foreach (KeyValuePair<char, int> c in countedCharacters)
+            {
+                Console.WriteLine($"{c.Key} : {c.Value}");
+            }
+
         }
     }
 }
