1ab77da1539b921f715d7180c7acc20654e08c29
Hanna Sumpter <sumpterhanna@gmail.com>
2020-10-12 11:13:39 -0500

    fixing git issue again

---
+++ Counting_Characters/Counting_Characters/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+public class Counter
+{
+	public static Dictionary<char, int> charCounter(string input)
+	{
+		input = input.ToLower();
+		Dictionary<char, int> countChars = new Dictionary<char, int>();
+		char[] chars = input.ToCharArray();
+
+		foreach (char c in chars)
+        {
+			if (char.IsLetter(c))
+            {
+				if (countChars.ContainsKey(c))
+                {
+					countChars[c]++;
+				}
+				else
+				{
+					countChars.Add(c, 1);
+				}
+			}
+        }
+
+		return countChars;
+	}
+}
